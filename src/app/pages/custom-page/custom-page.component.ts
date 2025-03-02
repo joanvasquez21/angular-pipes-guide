69f75b955197fb53fcbba5e93f1bf051e1827891
joanvasquez21 <joanvrivas.21@gmail.com>
2025-03-02 22:08:43 +0100

    feat: add custom pipe for case transformation called togglecase

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,9 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe],
   templateUrl: './custom-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent { 
+
+  name = signal('joan');
+
+  upperCase = signal(true);
+
+  toggleUpperCase() {
+    this.upperCase.set(!this.upperCase());
+  }
+
+
+}
--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,7 @@
-import { I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe } from '@angular/common';
+import { AsyncPipe, I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { interval, tap } from 'rxjs';
 
 
 const client1 = {
@@ -19,57 +20,74 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe, KeyValuePipe, TitleCasePipe],
+  imports: [CardComponent,
+            I18nSelectPipe, 
+            I18nPluralPipe, 
+            SlicePipe, 
+            JsonPipe, 
+            KeyValuePipe, 
+            TitleCasePipe,
+            AsyncPipe
+          ],
   templateUrl: './uncommon-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class UncommonPageComponent {
 
   //i18nselect
-    client = signal(client1);
+  client = signal(client1);
 
-    //el pipe i18n funciona con un map
-    invitationMap = {
-      male: 'invitarlo',
-      female: 'invitarla'
+  //el pipe i18n funciona con un map
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient() {
+    if (this.client() == client1) {
+      this.client.set(client2)
+      return;
     }
+    this.client.set(client1);
+  }
 
-    changeClient(){
-      if(this.client() == client1){
-        this.client.set(client2)
-        return;
-      }
-      this.client.set(client1);
-    }
+  //i18n plural funciona cuando tenemos muchos datos es decir colecciones o diferentes numeros de elementos
+  clientsMap = signal({
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos dos clientes esperando',
+    other: 'tenemos # clientes esperando'
+  })
 
-    //i18n plural funciona cuando tenemos muchos datos es decir colecciones o diferentes numeros de elementos
-    clientsMap = signal({
-      '=0' : 'no tenemos ningun cliente esperando',
-      '=1' : 'tenemos un cliente esperando',
-      '=2' : 'tenemos dos clientes esperando',
-      other :'tenemos # clientes esperando'
-    })
-    
-    clients = signal([
-      'ana',
-      'maria',
-      'andrea',
-      'juan',
-      'carlos',
-      'pedro'
-    ]);
+  clients = signal([
+    'ana',
+    'maria',
+    'andrea',
+    'juan',
+    'carlos',
+    'pedro'
+  ]);
 
-    deleteClient(){
-      this.clients.update ( prev => prev.slice(1));
-    }
+  deleteClient() {
+    this.clients.update(prev => prev.slice(1));
+  }
 
-    //keyvaluepipe
-    profile = {
-      name: 'joan v',
-      age: 25,
-      address: 'barcelona, españa'
-    }
-    
+  //keyvaluepipe
+  profile = {
+    name: 'joan v',
+    age: 25,
+    address: 'barcelona, españa'
+  }
+  //async pipe
 
+  promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('tenemos data en la promesa')
+      console.log('promesa finalizada')
+    }, 3500)
+  })
 
+  myObservableTimer = interval(2000).pipe(
+    tap( (value) => console.log('tap', value) )
+  );
 }
